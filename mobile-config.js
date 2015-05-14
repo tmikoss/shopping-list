a6287e877d6e70df8b28d3bb85f859512993edd5
Toms Mikoss <toms.mikoss@gmail.com>
2015-05-14 22:45:33 +0300

    Android attempt

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.accessRule('http://whattobuy.meteor.com/*');
+
+App.info({
+  id: 'lv.mikoss.whattobuy',
+  version: '1.0.0',
+  name: 'WhatToBuy',
+  description: 'Shopping list',
+  author: 'Toms Mikoss',
+  email: 'toms@mikoss.lv',
+  website: 'http://whattobuy.meteor.com'
+});
